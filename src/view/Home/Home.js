9375f23e1bfb54aa67294bca91152a62657b0388
TzuHui <angel1996421@gmail.com>
2021-02-07 14:55:57 +0800

    change component to functional

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -19,55 +19,5 @@ const Home = () => {
     </div>  
   )
 }
-// class About extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       error: null,
-//       isLoaded: false,
-//       items: []
-//     };
-//   }
-//   fetchData() {
-//     fetch("https://randomuser.me/api/?results=12")
-//     .then(res => res.json())
-//     .then(
-//       (result) => {
-//         this.setState({
-//           isLoaded: true,
-//           items: result.results
-//         });
-//       },
-//       (error) => {
-//         this.setState({
-//           isLoaded: true,
-//           error
-//         });
-//       }
-//     )
-//   }
-//   componentDidMount() {
-//     this.fetchData()
-//   }
-//   render() {
-//     const { error, isLoaded, items } = this.state;
-//     if (error) {
-//       return <div>Error: {error.message}</div>;
-//     } else if (!isLoaded) {
-//       return <div className="container mt-4"><div>Loading...</div></div>;
-//     } else {
-//       return (
-//         <div className="container mt-4">
-//           <h1> About </h1>
-//           <div className="row">
-//             {items.map(item => (
-//               <Card key={item.cell} data={item} />
-//             ))}
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
-// }
 
 export default Home;
--- src/view/ToDo/TodoList.js
+++ src/view/ToDo/TodoList.js
@@ -1,83 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState, useMemo } from 'react';
 import Todo from '../../components/Todo/Todo';
 
-class TodoList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [
-        { id: 1, text: 'Write Some Code', active: false },
-        { id: 2, text: 'Running', active: false },
-        { id: 3, text: 'Play Baskebtall', active: false },
-      ],
-      filterType: 'all',
-      inputValue: '',
-    };
-    this.changeState = this.changeState.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+const TodoList = () => {
+  const [filterType, setFilterType] = useState('all');
+
+  const [inputValue, setInputValue] = useState('');
+  function handleChange(event) {
+    setInputValue(event.target.value);
   }
-  handleChange(event) {
-    this.setState({ inputValue: event.target.value });
+
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Write Some Code', active: false },
+    { id: 2, text: 'Running', active: false },
+    { id: 3, text: 'Play Baskebtall', active: false },
+  ])
+
+  function addTodo() {
+    if (inputValue !== '') {
+      const todoLength = todos.length;
+      setTodos([...todos, {
+        id: todoLength + 1,
+        text: inputValue,
+        active: false
+      }])
+      setInputValue('');
+    }
   }
-  changeState(event) {
-    let todosList = this.state.todos;
-    const index = this.state.todos.findIndex(data => data.id === parseInt(event.target.id));
+  function changeState(event) {
+    let todosList = todos;
+    const index = todos.findIndex(data => data.id === parseInt(event.target.id));
     if (todosList[index].active) {
       todosList[index].active = false
     } else {
       todosList[index].active = true
     }
-    this.setState({ todos: todosList })
+    setTodos([...todosList]);
   }
-  addTodo() {
-    const todoLength = this.state.todos.length;
-    this.state.todos.push({
-      id: todoLength + 1,
-      text: this.state.inputValue,
-      active: false
-    });
-    this.setState({ todos: this.state.todos });
-    this.setState({ inputValue: '' });
-  }
-  get displayData() {
-    if (this.state.filterType !== 'all') {
-      if (this.state.filterType === 'completed') {
-        return this.state.todos.filter(todo => todo.active)
+  const displayData = useMemo(() => {
+    if (filterType !== 'all') {
+      if (filterType === 'completed') {
+        return todos.filter(todo => todo.active)
       } else {
-        return this.state.todos.filter(todo => !todo.active)
+        return todos.filter(todo => !todo.active)
       }
     } else {
-      return this.state.todos
+      return todos
     }
-  }
-  render() {
-    return (
-      <div className="py-md-5 py-3 px-md-4 w-100">
-        <h2 className="text-primary h1">Task List</h2>
-        <div className="input-group mt-md-5 mt-3">
-          <input type="text" className="form-control" placeholder="新增 Todo" value={this.state.inputValue} onChange={this.handleChange} />
-          <button className="btn btn-primary" type="button" id="button-addon2" onClick={() => this.addTodo()}>Add</button>
-        </div>
-        <div className="btn-group mb-3 mt-md-5 mt-3" role="group" aria-label="Basic outlined example">
-          <button type="button"
-            className={'btn btn-outline-primary ' + (this.state.filterType === "all" ? "active" : "")}
-            onClick={() => this.setState({ filterType: 'all' })}>全部</button>
-          <button type="button"
-            className={'btn btn-outline-primary ' + (this.state.filterType === "left" ? "active" : "")}
-            onClick={() => this.setState({ filterType: 'left' })}>未完成</button>
-          <button type="button"
-            className={'btn btn-outline-primary ' + (this.state.filterType === "completed" ? "active" : "")}
-            onClick={() => this.setState({ filterType: 'completed' })}>完成</button>
-        </div>
-        <ul className="list-group">
-          {this.displayData.map((value, key) => {
-            return <Todo data={value} id={value.id} key={key} handler={this.changeState} />
-          })}
-        </ul>
-        <p className="mt-3 text-primary text-right"> 共有 {this.state.todos.length} 項任務</p>
+  }, [filterType, todos]);
+
+  return (
+    <div className="py-md-5 py-3 px-md-4 w-100">
+      <h2 className="text-primary h1">Task List</h2>
+      <div className="input-group mt-md-5 mt-3">
+        <input type="text" className="form-control" placeholder="新增 Todo" value={inputValue} onChange={handleChange} />
+        <button className="btn btn-primary" type="button" id="button-addon2" onClick={() => addTodo()}>Add</button>
       </div>
-    );
-  }
+      <div className="btn-group mb-3 mt-md-5 mt-3" role="group" aria-label="Basic outlined example">
+        <button type="button"
+          className={'btn btn-outline-primary ' + (filterType === "all" ? "active" : "")}
+          onClick={() => setFilterType('all')}>全部</button>
+        <button type="button"
+          className={'btn btn-outline-primary ' + (filterType === "left" ? "active" : "")}
+          onClick={() => setFilterType('left')}>未完成</button>
+        <button type="button"
+          className={'btn btn-outline-primary ' + (filterType === "completed" ? "active" : "")}
+          onClick={() => setFilterType('completed')}>完成</button>
+      </div>
+      <ul className="list-group">
+        {displayData.map((value, key) => {
+          return <Todo data={value} id={value.id} key={key} handler={changeState} />
+        })}
+      </ul>
+      <p className="mt-3 text-primary text-right"> 共有 {todos.length} 項任務</p>
+    </div>
+  );
 }
 
 export default TodoList;
